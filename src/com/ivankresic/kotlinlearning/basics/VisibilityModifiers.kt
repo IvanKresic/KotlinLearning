a58ee818d35726acc0e8e2b0599a7a28679f938f
ivan <ivan.kresic@amodo.eu>
2020-05-29 15:01:49 +0200

    VisibilityModifiers

---
+++ src/com/ivankresic/kotlinlearning/basics/VisibilityModifiers.kt
@@ -0,0 +1,19 @@
+package com.ivankresic.kotlinlearning.basics
+
+/**
+ * Created by ivan on 29/05/2020
+ */
+
+/**
+ * There are 4 visibility modifiers in Kotlin
+ * public -> default and everywhere available
+ *
+ * Top level declarations:
+ * private -> available inside file containing declaration
+ * internal -> anywhere in the same module // maven moudule, gradle module, intelliJ module, Android Studio module ...
+ *
+ * Classes:
+ * private -> only available to class members
+ * protected -> same as private and subclasses
+ * internal -> any client inside module
+ * */
